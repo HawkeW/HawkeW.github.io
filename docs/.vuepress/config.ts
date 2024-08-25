41058587196c76349313ea95d106dbe0613c9c72
hughew <897878405@QQ.COM>
2024-08-26 00:37:16 +0800

    feat: unocss支持

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,11 +1,18 @@
 import { viteBundler } from '@vuepress/bundler-vite'
+import UnoCSS from 'unocss/vite'
 import { defineUserConfig } from "vuepress/cli";
 import theme from "./theme.js";
+import { getDirname, path } from "vuepress/utils";
+const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
   base: "/",
   bundler: viteBundler({
-    viteOptions: {},
+    viteOptions: {
+      plugins: [
+        UnoCSS(),
+      ]
+    },
     vuePluginOptions: {},
   }),
   dest: "./dist",
